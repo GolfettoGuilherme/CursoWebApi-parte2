9697ed90d06342db83991d471e85affc92992b97
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2020-01-12 20:25:20 -0300

    final do segundo curso

---
+++ Alura.WebAPI.WebApp/HttpClients/AuthApiClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Seguranca;
+
+namespace Alura.WebAPI.WebApp.HttpClients
+{
+
+    public class LoginResult
+    {
+        public bool Succeded { get; set; }
+        public string Token { get; set; }
+
+    }
+
+    public class AuthApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<LoginResult> PostLoginAsync(LoginModel model)
+        {
+            var resposta = await _httpClient.PostAsJsonAsync("login", model);
+            return new LoginResult
+            {
+                Succeded = resposta.IsSuccessStatusCode,
+                Token = await resposta.Content.ReadAsStringAsync()
+            };
+            
+        }
+    }
+}
---
+++ Alura.WebAPI.WebApp/HttpClients/LivroApiClient.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Seguranca;
+using Alura.WebAPI.WebApp.HttpClients;
+using Microsoft.AspNetCore.Http;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.ListaLeitura.HttpClients
+{
+    public class LivroApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _accessor;
+
+        public LivroApiClient(HttpClient httpClient, IHttpContextAccessor accessor)
+        {
+            _httpClient = httpClient;
+            _accessor = accessor;
+        }
+
+        private void AddBearerToken()
+        {
+            var token = _accessor.HttpContext.User.Claims.First(c => c.Type == "Token").Value;
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        public async Task<Lista> GetListaLeituraAsync(TipoListaLeitura tipo)
+        {
+
+            AddBearerToken();
+
+            var resposta = await _httpClient.GetAsync($"listasleitura/{tipo}");
+            resposta.EnsureSuccessStatusCode();
+            return await resposta.Content.ReadAsAsync<Lista>();
+        }
+
+        public async Task DeleteLivroAsync(int id)
+        {
+            AddBearerToken();
+            var respota = await _httpClient.DeleteAsync($"livros/{id}");
+            respota.EnsureSuccessStatusCode();
+        }
+
+        public async Task<LivroApi> GetLivroAsync(int id)
+        {
+            AddBearerToken();
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/{id}");
+
+            resposta.EnsureSuccessStatusCode(); //SE FOR 200, faz nada, se não for, vai gerar uma excessao
+
+            return await resposta.Content.ReadAsAsync<LivroApi>();
+        }
+
+        public async Task<byte[]> GetCapaAsync(int id)
+        {
+            AddBearerToken();
+
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/{id}/capa");
+
+            resposta.EnsureSuccessStatusCode(); //SE FOR 200, faz nada, se não for, vai gerar uma excessao
+
+            return await resposta.Content.ReadAsByteArrayAsync();
+        }
+
+        public async Task PostLivroAsync(LivroUpload model)
+        {
+            AddBearerToken();
+            HttpContent content = CreateMultipartFormDataContent(model);
+            var resposta = await _httpClient.PostAsync("livros", content);
+            resposta.EnsureSuccessStatusCode();
+        }
+
+        private string EnvolveComAspasDuplas(string valor)
+        {
+            return $"\"{valor}\"";
+        }
+
+        private HttpContent CreateMultipartFormDataContent(LivroUpload model)
+        {
+            //toda essa volta é para enviar um arquivo
+            var content = new MultipartFormDataContent();
+
+            content.Add(new StringContent(model.Titulo), EnvolveComAspasDuplas("titulo"));
+            content.Add(new StringContent(model.Lista.ParaString()), EnvolveComAspasDuplas("lista"));
+
+            if (!string.IsNullOrEmpty(model.Subtitulo))
+            {
+                content.Add(new StringContent(model.Subtitulo), EnvolveComAspasDuplas("subtitulo"));
+            }
+
+            if (!string.IsNullOrEmpty(model.Resumo))
+            {
+                content.Add(new StringContent(model.Resumo), EnvolveComAspasDuplas("resumo"));
+            }
+
+            if (!string.IsNullOrEmpty(model.Autor))
+            {
+                content.Add(new StringContent(model.Resumo), EnvolveComAspasDuplas("autor"));
+            }
+            
+
+            if(model.Id >0)
+            {
+                content.Add(new StringContent(model.Id.ToString()), EnvolveComAspasDuplas("id"));
+            }
+
+            if(model.Capa != null)
+            {
+                var imageContent = new ByteArrayContent(model.Capa.ConvertToBytes());
+                imageContent.Headers.Add("content-type", "image/png");
+                content.Add(imageContent, EnvolveComAspasDuplas("capa"),EnvolveComAspasDuplas("capa.png"));
+            }
+
+
+            return content;
+        }
+
+        public async Task PutLivroAsync(LivroUpload model)
+        {
+            AddBearerToken();
+            HttpContent content = CreateMultipartFormDataContent(model);
+            var resposta = await _httpClient.PutAsync("livros", content);
+            resposta.EnsureSuccessStatusCode();
+        }
+    }
+}
